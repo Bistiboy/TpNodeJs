df28ec7dcd6819d297f9d4a4dd8c68a8df4eab39
Bistiboy <nicolas.bisti@gmail.com>
2019-06-23 18:33:51 +0200

    Modication du port

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 const app = require('./app/app');
 const { logger } = require('./app/common/logger');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => logger.info(`App listening on port ${port}!`));
